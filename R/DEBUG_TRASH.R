dd22f179f0fc7cba3424de13ba2bec4636cf403e
Sebastjan Senk <seba.senk@gmail.com>
2026-01-07 23:33:31 +0100

    test ce dela k sm rabu nov token XD

--- R/DEBUG_TRASH.R
+++ R/DEBUG_TRASH.R
@@ -9,6 +9,8 @@ source("R/05_simulation.R")
 BS_TABLE_S17 <- read.csv("basic_strategy.csv", stringsAsFactors = FALSE)
 BS_TABLE_H17 <- read.csv("basic_strategy_H17.csv", stringsAsFactors = FALSE)
 
+basic_action_bs(c(5,6), dealer_up = 11, can_double=TRUE, can_split=FALSE, can_surrender=TRUE,   bs_table  = BS_TABLE_H17)
+
 a  <- basic_action_bs(c(11,6), dealer_up = 6)  # A,6 vs 6
 b  <- basic_action_bs(c(11,6), dealer_up = 2)  # A,6 vs 2
 a2 <- basic_action_bs(c(11,7), dealer_up = 6)  # A,7 vs 6
